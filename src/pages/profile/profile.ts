94066d766c25bf3e0c20d77f786087b90c713938
Guilherme Silva <gg-silva@outlook.com>
2019-08-03 10:27:04 -0300

    Criando uma página de profile

---
+++ src/pages/profile/profile.ts
@@ -0,0 +1,35 @@
+import { StorageService } from './../../services/storage.service';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ProfilePage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-profile',
+  templateUrl: 'profile.html',
+})
+export class ProfilePage {
+
+  email: string;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public storageService: StorageService
+  ) { }
+
+  ionViewDidLoad() {
+    let user = this.storageService.getLocalUser()
+    if (user && user.email)
+      this.email = user.email;
+
+    console.log('ionViewDidLoad ProfilePage');
+  }
+
+}
